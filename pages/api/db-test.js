ecf684a3e3f355ff22367924b86a9b8a95723572
SoulFreq <higherselfstudi01@gmail.com>
2025-09-30 17:26:17 -0400

    Delete pages directory

--- pages/api/db-test.js
+++
@@ -1,9 +0,0 @@
-import { query } from '../../lib/db';
-export default async function handler(req, res) {
-  try {
-    const r = await query('SELECT 1 as ok');
-    res.status(200).json({ ok: !!r });
-  } catch (e) {
-    res.status(500).json({ ok: false, error: e.message });
-  }
-}
--- pages/api/products.js
+++
@@ -1,5 +0,0 @@
-import { query } from '../../lib/db';
-export default async function handler(req, res) {
-  const result = await query('SELECT id, title FROM products', []);
-  res.status(200).json({ products: result.rows });
-}
